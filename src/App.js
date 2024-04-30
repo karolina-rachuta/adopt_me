2c2dd8f5629cf5b822cf0e3f4980e3554e35cdca
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-04-30 17:53:11 +0400

    last changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from "react";
 import Pet from "./components/Pet";
 
 function App() {
     return (
         <div>
-            <h1>"Adopt me!</h1>
+            <h1>Adopt me!</h1>
             <Pet name="Luna" animal="Dog" breed="Havanese"/>
             <Pet name="Pepper" animal="Bird" breed="Cockatiel"/>
         </div>

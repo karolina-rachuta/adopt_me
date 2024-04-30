ba7f7401146db2f06d82c1afe5f81516d0656507
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-04-30 19:34:17 +0400

    added serach by animal kind

--- src/components/SearchParams.jsx
+++ src/components/SearchParams.jsx
@@ -1,11 +1,45 @@
+import { useState } from "react";
+
+const animals = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 function SearchParams() {
-  const location = "Seattle, WA";
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
 
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">Location</label>
-        <input type="text" id="location" value={location} placeholder="Location"/>
+        <input
+          type="text"
+          id="location"
+          value={location}
+          placeholder="Location"
+          onChange={(e) => {
+            setLocation(e.target.value);
+          }}
+        />
+
+        <label htmlFor="animal">Animal</label>
+        <select
+          id="animal"
+          value={animal}
+          onChange={(e) => {
+            setAnimal(e.target.value);
+          }}
+          onBlur={(e) => {
+            setAnimal(e.target.value);
+          }}
+        >
+          <option />
+          {animals.map((animal) => (
+            <option value={animal} key={animal}>
+              {animal}
+            </option>
+          ))}
+          ;
+        </select>
+
         <button type="submit">Submit</button>
       </form>
     </div>

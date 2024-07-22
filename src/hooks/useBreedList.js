4b454cbed14f548d4b83bb89e1fa4b499c9c9754
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-07-22 13:42:27 +0400

    removing comments

--- src/hooks/useBreedList.js
+++ src/hooks/useBreedList.js
@@ -5,14 +5,11 @@ import {
 
 const localCache = {};
 
-//ma zgarniac liste breedow w zaleznosci od animal
+
 function useBreedList(animal) {
-    //potrzebujemy gdzies trzymac liste tych wszystkich ras:
     const [breedList, setBreedList] = useState([]);
     const [status, setStatus] = useState("unloaded");
 
-    //use effect aby wywolac request
-    // jezeli mamy juz zapisane breeds dla danego animala bierzemy z localCache, jesli nie to robimy requests do servera
     useEffect(() => {
         if (!animal) {
             setBreedList([]);
@@ -28,14 +25,11 @@ function useBreedList(animal) {
         setStatus("loading")
         const response = await fetch(`http://pets-v2.dev-apis.com/breeds?animal=${animal}`);
         const data = await response.json();
-        //memoizacja
         localCache[animal] = data.breeds;
-        //jesli data.breeds jest undefined to trzeba zaladowac mu pusta tablice
-        //zmiana na memoizacje i stan pobierany z cache
         setBreedList(localCache[animal]);
         setStatus("loaded");
     }
-    //bedzie dwuwymiarowa tablica
+
     return [breedList, status];
 };
 
